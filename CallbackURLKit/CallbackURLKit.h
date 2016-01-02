d386556bb2cbba42858a24e0a8dfeca087739273
phimage <ssjgotrunk@yahoo.fr>
2016-01-02 11:05:01 +0100

    Initial Commit

---
+++ CallbackURLKit/CallbackURLKit.h
@@ -0,0 +1,19 @@
+//
+//  CallbackURLKit.h
+//  CallbackURLKit
+//
+//  Created by phimage on 02/01/16.
+//  Copyright © 2016 phimage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CallbackURLKit.
+FOUNDATION_EXPORT double CallbackURLKitVersionNumber;
+
+//! Project version string for CallbackURLKit.
+FOUNDATION_EXPORT const unsigned char CallbackURLKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CallbackURLKit/PublicHeader.h>
+
+
