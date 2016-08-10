da7079fc87b2b640c1b56dc3c12a2ea1979d77e1
Chris Patterson <christopher.patterson@lilly.com>
2016-08-10 17:44:28 -0400

    Try again to encode ampersands in callback query args.

--- CallbackURLKit/Manager.swift
+++ CallbackURLKit/Manager.swift
@@ -201,15 +201,15 @@ public class Manager {
             let xcuParams: Parameters = [kRequestID: request.ID]
             
             if request.successCallback != nil {
-                xcuComponents.query = (xcuParams + [kResponseType: ResponseType.success.rawValue]).query.ampersandEncoded
-                query[kXCUSuccess] = xcuComponents.URL?.absoluteString ?? ""
+                xcuComponents.query = (xcuParams + [kResponseType: ResponseType.success.rawValue]).query
+                query[kXCUSuccess] = xcuComponents.URL?.absoluteString.ampersandEncoded ?? ""
                 
-                xcuComponents.query = (xcuParams + [kResponseType: ResponseType.cancel.rawValue]).query.ampersandEncoded
-                query[kXCUCancel] = xcuComponents.URL?.absoluteString ?? ""
+                xcuComponents.query = (xcuParams + [kResponseType: ResponseType.cancel.rawValue]).query
+                query[kXCUCancel] = xcuComponents.URL?.absoluteString.ampersandEncoded ?? ""
             }
             if request.failureCallback != nil {
-                xcuComponents.query = (xcuParams + [kResponseType: ResponseType.error.rawValue]).query.ampersandEncoded
-                query[kXCUError] = xcuComponents.URL?.absoluteString ?? ""
+                xcuComponents.query = (xcuParams + [kResponseType: ResponseType.error.rawValue]).query
+                query[kXCUError] = xcuComponents.URL?.absoluteString.ampersandEncoded ?? ""
             }
             
             if request.hasCallback {
